cec59369ad3dd0265afda31699ce2b9d7f964f6b
ShadowOfSokol <sokolenkoegor19702002@gmail.com>
2025-01-25 16:57:32 +0200

    add diograms and make cod for main2.py

--- main2.py
+++ main2.py
@@ -5,3 +5,52 @@
 та доходом, а також дозволяє сегментувати відвідувачів за їхньою поведінкою
 (частота візитів і середня сума замовлення).
 '''
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.cluster import KMeans
+
+def main():
+    # Data generation
+    np.random.seed(42)
+    visitors = np.random.randint(6,45, 30)
+    revenue = 10 * visitors + np.random.randint(600,9000, 30)
+    data = {
+        "Visitors": visitors,
+        "Revenue": revenue,
+    }
+    df = pd.DataFrame(data)
+
+    # Data divide
+    X = df[["Visitors"]]
+    Y = df["Revenue"]
+
+    # Model
+    model = LinearRegression()
+    model.fit(X, Y)
+    # Prediction
+    Y_pred = model.predict(X)
+
+    # Visualization
+    plt.scatter(X, Y, color="blue", label = "Data")
+    plt.plot(X, Y_pred, color="red", label="Prediction")
+    plt.title("Finale Revenue Amount")
+    plt.xlabel("Visitors")
+    plt.ylabel("Revenue")
+    plt.legend()
+    plt.show()
+
+
+
+    # Clusterization
+
+    # Data divide
+
+    # Model
+
+    # Prediction
+
+if __name__ == '__main__':
+    main()
